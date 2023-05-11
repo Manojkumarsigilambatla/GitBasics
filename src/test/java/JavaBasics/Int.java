22bc5cbc9eec8ee6272f96faeb18af12d68b310e
MK <mks@gmail.com>
2023-05-11 15:07:43 +0530

    manoj's main workspace changed the code

---
+++ src/test/java/JavaBasics/Int.java
@@ -0,0 +1,18 @@
+package JavaBasics;
+
+import java.util.Scanner;
+
+public class Int {
+	
+	public static int i=10;
+	public static void main(String[] args) {
+		System.out.println(i);
+		Scanner sc = new Scanner(System.in);
+		int i=sc.nextInt();
+		//sc.nextLine();
+		String s=sc.nextLine();
+		
+		System.out.println(s +" "+ i);
+	}
+
+}
